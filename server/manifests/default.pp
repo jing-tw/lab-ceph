826f6fe396eafc5a1a1407c9efcb9054ba7ff59a
jing-tw <mqjing@gmail.com>
2015-12-24 14:19:34 +0800

    [add] bash script to install ceph-deploy code

--- server/manifests/default.pp
+++ server/manifests/default.pp
@@ -9,6 +9,15 @@ node default {
   file { "/etc/hostname": 		content => $myhostname; 	}
   exec { "setup hostname":		command => "/bin/hostname ${myhostname}",	}
   
+  exec { 'Install Infernalis':              
+    command => '/bin/echo Install Ceph-deploy',
+    onlyif => [
+      "/usr/bin/wget -q -O- 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc' | sudo apt-key add -" ,
+      "/bin/echo deb http://ceph.com/debian-infernalis/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list",
+      "/usr/bin/apt-get update && /usr/bin/apt-get install ceph-deploy -y"
+    ]
+  }
+  
   # setup address mapping
   include dnsmasq
   dnsmasq::address { $myhostname:	ip  => '192.168.50.2',  }  
@@ -16,3 +25,4 @@ node default {
   dnsmasq::address { "node2":		ip  => '192.168.50.11',  }
   dnsmasq::address { "node3":		ip  => '192.168.50.12',  }
 }
+
