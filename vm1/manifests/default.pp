80ef42011d47dc379062d4c594b8c2c03732a27e
jing-tw <mqjing@gmail.com>
2015-12-25 15:25:21 +0800

    Ready for ceph deploy

--- vm1/manifests/default.pp
+++ vm1/manifests/default.pp
@@ -1,15 +1,18 @@
-# Setup hostname and domain name server
-# by Jing.
+# ready for ceph installation
+  exec { 'ready ceph':
+    command => '/bin/echo Ready for Ceph-deploy',
+    onlyif => [
+      "/usr/bin/apt-get update && /usr/bin/apt-get install ntp openssh-server -y"
+    ]
+  }
 
-node default {
-
-  $myhostname = "node1"
-  $domain_name_server_ip="192.168.50.2"
-  
-  # setup name
-  file { "/etc/hostname": 		content => $myhostname; 	}
-  exec { "setup hostname":		command => "/bin/hostname ${myhostname}",	}
-  
-  # setup customized DNS server
-  file { "/etc/resolv.conf": content => "nameserver ${domain_name_server_ip}"; }
-}
+  # add ceph-user for installation
+  exec { 'Add user':
+    command => '/bin/echo add management user OK',
+    onlyif => [
+      "/usr/sbin/useradd -d /home/ceph-user -m ceph-user",
+      "/bin/echo ceph-user:1234 | chpasswd",
+      "/bin/echo \"ceph-user ALL = (root) NOPASSWD:ALL\" | sudo tee /etc/sudoers.d/ceph-user",
+      "/bin/chmod 0440 /etc/sudoers.d/ceph-user"
+    ]
+  }
