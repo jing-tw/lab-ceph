1e7b485a283da2f68ba59849815674a43f72a7d6
jing-tw <mqjing@gmail.com>
2016-04-27 11:56:28 +0800

    rm files

--- misc/restore_key.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir /etc/ceph
-mkdir /var/lib/ceph
-
-cp -fr ./1/* /etc/ceph/
-cp -fr ./2/* /var/lib/ceph/
--- misc/save_key.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -fr ./1; mkdir 1
-rm -fr ./2; mkdir 2
-cp -fr /etc/ceph/* ./1
-cp -fr /var/lib/ceph/* ./2
